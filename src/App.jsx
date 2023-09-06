3f422d4889f0043def3453308cc665651d39f439
priyanshi <priyanshikhippal6482@gmail.com>
2023-09-07 03:53:39 +0530

    updaeted customize page

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,13 @@
-
+import Canvas from './canvas';
+import Customizer from './pages/Customizer';
+import Home from './pages/Home';
 import './App.css'
-import Canvas from './canvas'
-import Customizer from './pages/Customizer'
-import Home from './pages/Home'
-
 function App() {
-
-
   return (
-    <main>
+    <main className="app transition-all ease-in">
       <Home />
       <Canvas />
       <Customizer />
-    
     </main>
   )
 }
